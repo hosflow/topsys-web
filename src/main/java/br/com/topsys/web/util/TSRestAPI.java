4a7408937650fd109675a19e15ac0d7b582a25c4
André Topázio <andre.topazio@gmail.com>
2021-02-11 18:43:47 -0300

    ...

--- src/main/java/br/com/topsys/web/util/TSRestAPI.java
+++ src/main/java/br/com/topsys/web/util/TSRestAPI.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -28,7 +27,6 @@ public final class TSRestAPI<T extends Serializable> {
 
 	private static final String NAO_PODE_SER_NULO = "O objeto passado por parâmetro do método post não pode ser nulo!";
 
-	@Value("${smpep.base.url}")
 	private String baseURL;
 
 	private RestTemplate restTemplate;
@@ -37,15 +35,8 @@ public final class TSRestAPI<T extends Serializable> {
 		this.baseURL = baseURL;
 		this.restTemplate = new RestTemplate();
 		this.restTemplate.setErrorHandler(new TSRestResponseException());
-
 	}
 
-	public TSRestAPI() {
-		this.restTemplate = new RestTemplate();
-		this.restTemplate.setErrorHandler(new TSRestResponseException());
-
-	}
-	
 	public T post(Class<T> classe, String url, Object object) {
 
 		T retorno = null;
@@ -53,16 +44,14 @@ public final class TSRestAPI<T extends Serializable> {
 		HttpEntity<Object> entity = null;
 
 		ObjectMapper objectMapper = null;
-	
+
 		try {
 			if (TSUtil.isEmpty(object)) {
 				throw new TSSystemException(NAO_PODE_SER_NULO);
 			}
+
 			
-			HttpHeaders headers = new HttpHeaders();
-			headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
-			
-			entity = new HttpEntity<Object>(object,headers);
+			entity = new HttpEntity<Object>(object);
 
 			retorno = restTemplate.postForObject(this.getBaseURL() + url, entity, classe);
 
@@ -79,7 +68,7 @@ public final class TSRestAPI<T extends Serializable> {
 
 		return retorno;
 	}
-	
+
 	public Object postObject(Class<?> classe, String url, Object object) {
 
 		Object retorno = null;
@@ -87,16 +76,14 @@ public final class TSRestAPI<T extends Serializable> {
 		HttpEntity<Object> entity = null;
 
 		ObjectMapper objectMapper = null;
-	
+
 		try {
 			if (TSUtil.isEmpty(object)) {
 				throw new TSSystemException(NAO_PODE_SER_NULO);
 			}
+
 			
-			HttpHeaders headers = new HttpHeaders();
-			headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
-			
-			entity = new HttpEntity<Object>(object,headers);
+			entity = new HttpEntity<Object>(object);
 
 			retorno = restTemplate.postForObject(this.getBaseURL() + url, entity, classe);
 
@@ -113,7 +100,6 @@ public final class TSRestAPI<T extends Serializable> {
 
 		return retorno;
 	}
-
 
 	@SuppressWarnings("unchecked")
 	public List<T> postList(Class<T> classe, String url, Object object) {
@@ -130,11 +116,9 @@ public final class TSRestAPI<T extends Serializable> {
 			if (TSUtil.isEmpty(object)) {
 				throw new TSSystemException(NAO_PODE_SER_NULO);
 			}
-			
-			HttpHeaders headers = new HttpHeaders();
-			headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
-			
-			entity = new HttpEntity<Object>(object,headers);
+
+		
+			entity = new HttpEntity<Object>(object);
 
 			retorno = restTemplate.postForObject(this.getBaseURL() + url, entity, List.class);
 
