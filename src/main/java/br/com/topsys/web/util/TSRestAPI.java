6e676bbb5aed1c7e81a6c70bbc412a425ff6c168
André Topázio <andre.topazio@gmail.com>
2021-02-26 12:05:13 -0300

    ...

--- src/main/java/br/com/topsys/web/util/TSRestAPI.java
+++ src/main/java/br/com/topsys/web/util/TSRestAPI.java
@@ -17,7 +17,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.CollectionType;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import br.com.topsys.base.exception.TSApplicationException;
