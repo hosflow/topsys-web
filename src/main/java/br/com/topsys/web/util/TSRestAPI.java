0c4660fc34c571dabb02b89476508cf0513fd4b1
André Topázio <andre.topazio@gmail.com>
2021-07-29 16:00:14 -0300

    ...

--- src/main/java/br/com/topsys/web/util/TSRestAPI.java
+++ src/main/java/br/com/topsys/web/util/TSRestAPI.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import br.com.topsys.base.exception.TSApplicationException;
@@ -209,6 +210,7 @@ public final class TSRestAPI {
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		objectMapper.registerModule(new JavaTimeModule());
 		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
 		objectMapper.setSerializationInclusion(Include.NON_NULL);
 		
 		return objectMapper; 
