500e2f6e8d8bc9df69efe9c644bdaa7d25334569
André Topázio <andre.topazio@gmail.com>
2022-06-15 12:55:32 -0300

    ...

--- src/main/java/br/com/topsys/web/facade/TSServiceFacade.java
+++ src/main/java/br/com/topsys/web/facade/TSServiceFacade.java
@@ -1,7 +1,10 @@
 package br.com.topsys.web.facade;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.topsys.base.model.TSRestModel;
 import br.com.topsys.web.util.TSRestAPI;
 import lombok.Data;
 
@@ -19,6 +22,20 @@ public abstract class TSServiceFacade {
 	
 	public TSServiceFacade(String token) {
 		this.token = token;
+	}
+	
+	
+	public <T> T post(Class<T> classe, Object model, String endpoint) {
+
+		return this.getRestAPI().post(classe, TSRestModel.builder().model(model).url(endpoint).token(this.getToken()).build());
+
+	}
+	
+	public <T> List<T> list(Class<T> classe, Object model, String endpoint) {
+
+		return this.getRestAPI().postList(classe, TSRestModel.builder().model(model).url(endpoint).token(this.getToken()).build());
+
+
 	}
 
 }
